fa04d317e86f6fd71ac05347d0b3ae9ed45cd096
HKobe <HKobeissi1@hotmail.com>
2016-05-24 12:52:28 -0400

    Initial Commit

---
+++ Lab2_HassanKobeissi/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ Date Created = 5/24/16
+ * Version = 0.0.1
+ 
+ */
+namespace Lab2_HassanKobeissi
+{
+    class Person
+    {
+        Name();
+        private static string Name()
+        {
+            string name = "Tom";
+            return  name;
+    }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
